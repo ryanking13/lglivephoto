40c4b45864c31717267141c8922e2bc02b76a680
Gyeongjae Choi <def6488@gmail.com>
2021-06-04 06:13:31 +0000

    Add docstrings

--- lglivephoto.go
+++ lglivephoto.go
@@ -1,3 +1,7 @@
+// Package lglivephoto provides functions to unpack or pack LG Live Photo.
+// LG Live Photo is a functionality in LG smartphones
+// where actions before and after taking a photo is recorded with the photo.
+// (see: https://www.lg.com/uk/support/product-help/CT00008356-20150844039308)
 package lglivephoto // import "github.com/ryanking13/lglivephoto"
 
 import (
@@ -6,6 +10,8 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+// Unpack takes a Live Photo file and extracts it to
+// image and video embedded in the photo.
 func Unpack(imagePath string) ([]byte, []byte, error) {
 	Logger.Debugf("Start unpacking: %s", imagePath)
 	data, err := ioutil.ReadFile(imagePath)
@@ -24,6 +30,7 @@ func Unpack(imagePath string) ([]byte, []byte, error) {
 	return data[:idx], data[idx:], nil
 }
 
+// Pack generates LG Live Photo by embedding the mp4 video to the image,
 func Pack(imagePath string, videoPath string) ([]byte, error) {
 	Logger.Debugf("Start packing: %s to %s", videoPath, imagePath)
 	return nil, nil
